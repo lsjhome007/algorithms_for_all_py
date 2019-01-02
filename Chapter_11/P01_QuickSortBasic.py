790c9b10c2ca0983f6d827adc801f6adb70c510a
lsjhome <lsjhome007@gmail.com>
2019-01-02 23:32:52 +0900

    Chapter 11 add

---
+++ Chapter_11/P01_QuickSortBasic.py
@@ -0,0 +1,28 @@
+def quick_sort(a):
+
+    n = len(a)
+
+    if n <= 1:
+
+        return a
+
+    pivot = a[-1]
+
+    g1 = []
+    g2 = []
+
+    for i in range(0, n-1):
+
+        if a[i] < pivot:
+            g1.append(a[i])
+        else:
+            g2.append(a[i])
+
+    return quick_sort(g1) + [pivot] + quick_sort(g2)
+
+
+if __name__ == '__main__':
+
+    d = [6, 8, 3, 9, 10, 1, 2, 4, 7, 5]
+
+    print (quick_sort(d))
